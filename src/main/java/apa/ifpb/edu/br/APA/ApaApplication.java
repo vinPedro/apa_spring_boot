a8a53e4212a52d149451aec8833e49ec596c7c55
Pedro Vinicius <159270159+vinPedro@users.noreply.github.com>
2025-10-26 07:45:38 -0300

    Primeiro Commit: commit com as dependências/recursos inicias do projeto.

---
+++ src/main/java/apa/ifpb/edu/br/APA/ApaApplication.java
@@ -0,0 +1,13 @@
+package apa.ifpb.edu.br.APA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/apa/ifpb/edu/br/APA/ApaApplicationTests.java
@@ -0,0 +1,13 @@
+package apa.ifpb.edu.br.APA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
