a1cd105309dc48bf0c10dcf965640c8016e3e8d0
reysoares <soaresreynaldo240@gmail.com>
2025-11-11 16:08:13 -0300

    Enviar exception pro front

--- src/main/java/apa/ifpb/edu/br/APA/exception/GlobalExceptionHandler.java
+++ src/main/java/apa/ifpb/edu/br/APA/exception/GlobalExceptionHandler.java
@@ -3,7 +3,9 @@ package apa.ifpb.edu.br.APA.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(RecursoNaoEncontradoException.class)
     public ResponseEntity<ErrorMessage> handleRecursoNaoEncontrado(RecursoNaoEncontradoException ex) {
